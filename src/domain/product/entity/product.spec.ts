4b5e8288e8ead72fe9bc08d7162645e012348428
Henrique Beltrami <henriquemunhoz@finchsolucoes.com.br>
2025-06-01 18:55:28 -0300

    correção desafio notification

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -13,6 +13,12 @@ describe("Product unit tests", () => {
     }).toThrowError("product: Name is required");
   });
 
+  it("should throw error when id and name are empty", () => {
+    expect(() => {
+      const product = new Product("", "", 100);
+    }).toThrowError("product: Id is required,product: Name is required");
+  });
+
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
@@ -29,11 +35,5 @@ describe("Product unit tests", () => {
     const product = new Product("123", "Product 1", 100);
     product.changePrice(150);
     expect(product.price).toBe(150);
-  });
-
-   it("should throw error when id and name are empty", () => {
-    expect(() => {
-      const product = new Product("", "", 100);
-    }).toThrowError("product: Id is required,product: Name is required");
   });
 });
